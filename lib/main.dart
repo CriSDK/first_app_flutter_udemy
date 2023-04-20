613f9144f21d11c65891884d988f99031296f1b4
BRALLAN <basca@rexmas.com>
2023-04-20 03:35:11 -0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'contador solo en android 022',
             ),
             Text(
               '$_counter',
